b71ae3b6f77a2a6da39dc71d2204bb1b91d9fd2e
Your Name <hardsontessi2@gmail.com>
2022-06-06 18:01:30 +0100

    maeven

--- src/main/java/com/hartech/securityservice/Security/JWTUtils.java
+++ src/main/java/com/hartech/securityservice/Security/JWTUtils.java
@@ -4,6 +4,6 @@ abstract public class JWTUtils {
     public static final String SECRET = "mySecret1234";
     public static final String PREFIX = "Bearer ";
     public static final String AUTH_HEADER = "Authorization";
-    public static final long EXPIRATION_ACCESS_TOKEN = 1 * 60 * 1000;
-    public static final long EXPIRATION_REFRESH_TOKEN = 15 * 60 * 1000;
+    public static final long EXPIRATION_ACCESS_TOKEN = 60 * 60 * 1000;
+    public static final long EXPIRATION_REFRESH_TOKEN = 120 * 60 * 1000;
 }
